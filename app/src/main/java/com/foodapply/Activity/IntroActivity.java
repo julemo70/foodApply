e14a4fc13558f6c38cbee72d035038be544fe1ac
julemo70 <kjule35@gmail.com>
2022-05-06 13:40:40 +0000

    Initial commit

---
+++ app/src/main/java/com/foodapply/Activity/IntroActivity.java
@@ -0,0 +1,30 @@
+package com.foodapply.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.foodapply.R;
+
+public class IntroActivity extends AppCompatActivity {
+    private TextView startBtn;
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/foodapply/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.foodapply.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.foodapply.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
